d3cb75890eb348e561ad37f1334a7e1af1ece89c
ananyeaah <ananyagupta319@gmail.com>
2017-03-02 21:33:12 +0530

    commit

---
+++ tutorial1/t1.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as numpy
+import matplotlib.pyplot as plt
+#filter 
+#if anything not specified read as coloured image
+#									0
+img=cv2.imread('a.jpg',cv2.IMREAD_GRAYSCALE)
+#IMREAD_COLOR=1
+#IMREAD_UNCHANGED=-1
+#kick out alpha-use grayscale instead of r,g,b,alpha
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite('a.jpg',img)
+#cv2.imshow('image',img)
+plt.imshow(img,cmap='gray',interpolation='bicubic')
+plt.plot([50,100],[80,100],'c',linewidth=5)
+plt.show()
